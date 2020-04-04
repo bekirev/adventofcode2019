7e19e9f1549458a1f184f061ed6759e377c71163
bekirev <abekirev@gmail.com>
2020-04-04 19:14:08 +0300

    day 1

---
+++ src/main/kotlin/Utils.kt
@@ -0,0 +1,34 @@
+import java.net.URI
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.stream.Stream
+
+object PathFinder {
+    private fun uriFromResources(strPath: String): URI = javaClass.classLoader.getResource(strPath).toURI()
+
+    fun fromResources(strPath: String): Path = Paths.get(uriFromResources(strPath))
+}
+
+fun linesFromResource(strPath: String): Stream<String> =
+    Files.lines(PathFinder.fromResources(strPath))!!
+
+fun linesFromResource(path: Path): Stream<String> =
+    linesFromResource(path.toString())
+
+fun longLinesStream(inputPath1: Path): Stream<List<Long>> {
+    return linesFromResource(inputPath1)
+        .map { line ->
+            line
+                .split("\t")
+                .map(String::toLong)
+        }
+}
+
+fun Stream<Long>.sum(): Long {
+    return reduce<Long>(
+        0,
+        { acc, value -> acc + value },
+        { acc1, acc2 -> acc1 + acc2 }
+    ) ?: 0
+}
---
+++ src/main/kotlin/adventofcode2019/day01/FuelCalculator.kt
@@ -0,0 +1,34 @@
+package adventofcode2019.day01
+
+import linesFromResource
+import sum
+import java.nio.file.Paths
+import kotlin.math.max
+
+fun main() {
+    println(
+        getInput()
+            .map(::requiredFuelByMass)
+            .sum()
+    )
+
+    println(
+        getInput()
+            .map(::totalRequiredFuelByMass)
+            .sum()
+    )
+}
+
+private fun getInput() = linesFromResource(Paths.get("adventofcode2019", "day01", "input.txt"))
+    .map(String::toLong)
+
+fun requiredFuelByMass(mass: Long): Long = max(0, mass / 3 - 2)
+
+fun totalRequiredFuelByMass(mass: Long): Long {
+    tailrec fun totalRequiredFuelByMass(mass: Long, totalFuel: Long): Long =
+        when (val additionalFuel = requiredFuelByMass(mass)) {
+            0L -> totalFuel + additionalFuel
+            else -> totalRequiredFuelByMass(additionalFuel, totalFuel + additionalFuel)
+        }
+    return totalRequiredFuelByMass(mass, 0)
+}
