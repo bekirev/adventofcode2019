86b84cfae28f195052380c535d59fd3d9d77dcff
bekirev <abekirev@gmail.com>
2020-04-29 10:12:06 +0300

    refactor to multi-project

--- intcode/build.gradle.kts
+++ intcode/build.gradle.kts
@@ -2,24 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.3.72"
-}
-
-group = "org.example.adventofcode2019"
-version = "1.0-SNAPSHOT"
-
-allprojects {
-    repositories {
-        mavenCentral()
-        jcenter()
-    }
+    kotlin("jvm")
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.5")
-    implementation("com.google.guava", "guava", "29.0-jre")
-    compile(project(":intcode"))
     compile(project(":util"))
     testCompile("junit", "junit", "4.12")
 }
